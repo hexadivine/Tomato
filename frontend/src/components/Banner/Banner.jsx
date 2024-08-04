9bf2bff522c0eae851bfe028fd13bf305979df4f
hexadivine <hexadivine@gmail.com>
2024-08-04 16:33:32 +1000

    feat(component): add banner

---
+++ frontend/src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div class="banner">
+            <p>Order your <br/> favourite food here</p>
+            <p>Choose from a diverse menu featuring a delectable array of dishes.<br/>Our mission is to satisfy your cravings and elevate your dining experience.<br/>One delicious meal at a time</p>
+            <input type="button" value="View Menu" />
+    </div>
+  )
+}
+
+export default Banner
