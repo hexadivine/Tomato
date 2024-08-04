a21af9f44242506984b07fbe178c0ee74523a61a
hexadivine <hexadivine@gmail.com>
2024-08-04 16:36:45 +1000

    feat: use react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Cart from "./pages/Cart/Cart"
+
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </>
   )
 }
