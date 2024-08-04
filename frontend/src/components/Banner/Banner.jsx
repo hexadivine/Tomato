deb747df6174b789700f7017477eb0e211bfa87e
hexadivine <hexadivine@gmail.com>
2024-08-04 21:39:47 +1000

    fix: make responsive

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,13 +1,21 @@
-import './Banner.css'
+import "./Banner.css";
 
 function Banner() {
-  return (
-    <div class="banner">
-            <p>Order your <br/> favourite food here</p>
-            <p>Choose from a diverse menu featuring a delectable array of dishes.<br/>Our mission is to satisfy your cravings and elevate your dining experience.<br/>One delicious meal at a time</p>
-            <input type="button" value="View Menu" />
-    </div>
-  )
+    return (
+        <div className="banner">
+            <div className="banner-content">
+                <p>
+                    Order your favourite food here
+                </p>
+                <p>
+                    Choose from a diverse menu featuring a delectable array of dishes.Our mission is
+                    to satisfy your cravings and elevate your dining experience.One delicious meal
+                    at a time
+                </p>
+                <input type="button" value="View Menu" />
+            </div>
+        </div>
+    );
 }
 
-export default Banner
+export default Banner;
