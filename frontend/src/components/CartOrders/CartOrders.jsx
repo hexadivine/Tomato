f229c56564623e7e8ef91ab77ab48a35ea1cbd3b
hexadivine <hexadivine@gmail.com>
2024-08-10 19:26:17 +1000

    update

---
+++ frontend/src/components/CartOrders/CartOrders.jsx
@@ -0,0 +1,60 @@
+import "./CartOrders.css";
+import { useCart } from "./../../contexts/Cart/CartContext";
+
+function CartOrders() {
+    const { getCartInfo, setCart, getTotal } = useCart();
+    const cartInfo = getCartInfo();
+    console.log(cartInfo);
+    console.log(getTotal());
+    return (
+        <div>
+            <table className="cart-table">
+                <thead>
+                    <th>Items</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Quntity</th>
+                    <th>Total</th>
+                    <th>Remove</th>
+                </thead>
+                <tbody>
+                    {cartInfo.map((cartItem, index) => (
+                        <tr key={index}>
+                            <td>
+                                <img src={cartItem.img} alt="" />
+                            </td>
+                            <td>{cartItem.title}</td>
+                            <td>${cartItem.price}</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={cartItem.quantity}
+                                    onChange={(e) =>
+                                        setCart((prev) => {
+                                            if (e.target.value >= 1 && e.target.value <= 100)
+                                                prev[cartItem.id] = e.target.value;
+                                            return { ...prev };
+                                        })
+                                    }
+                                />
+                            </td>
+                            <td>{cartItem.total}</td>
+                            <td>
+                                <button
+                                    onClick={() => setCart((prev) => {
+                                        delete prev[cartItem.id];
+                                        return { ...prev };
+                                    })}
+                                >
+                                    x
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default CartOrders;
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,60 +1,11 @@
-import "./Cart.css";
-import { useCart } from "./../../contexts/Cart/CartContext";
+import CartOrders from "../../components/CartOrders/CartOrders"
 
 function Cart() {
-    const { getCartInfo, setCart, getTotal } = useCart();
-    const cartInfo = getCartInfo();
-    console.log(cartInfo);
-    console.log(getTotal());
-    return (
-        <div>
-            <table className="cart-table">
-                <thead>
-                    <th>Items</th>
-                    <th>Title</th>
-                    <th>Price</th>
-                    <th>Quntity</th>
-                    <th>Total</th>
-                    <th>Remove</th>
-                </thead>
-                <tbody>
-                    {cartInfo.map((cartItem) => (
-                        <tr>
-                            <td>
-                                <img src={cartItem.img} alt="" />
-                            </td>
-                            <td>{cartItem.title}</td>
-                            <td>${cartItem.price}</td>
-                            <td>
-                                <input
-                                    type="text"
-                                    value={cartItem.quantity}
-                                    onChange={(e) =>
-                                        setCart((prev) => {
-                                            if (e.target.value >= 1 && e.target.value <= 100)
-                                                prev[cartItem.id] = e.target.value;
-                                            return { ...prev };
-                                        })
-                                    }
-                                />
-                            </td>
-                            <td>{cartItem.total}</td>
-                            <td>
-                                <button
-                                    onClick={() => setCart((prev) => {
-                                        delete prev[cartItem.id];
-                                        return { ...prev };
-                                    })}
-                                >
-                                    x
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <>
+        <CartOrders />
+    </>
+  )
 }
 
-export default Cart;
+export default Cart
