7333535a48fdf99139c476385a30d3d433cf680d
hexadivine <hexadivine@gmail.com>
2024-09-28 19:12:32 +1000

    feat: add food options

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -121,6 +121,11 @@ function Add() {
                             <option value="Salad">Salad</option>
                             <option value="Rolls">Rolls</option>
                             <option value="Deserts">Deserts</option>
+                            <option value="Sandwich">Sandwich</option>
+                            <option value="Cake">Cake</option>
+                            <option value="Pure Veg">Pure Veg</option>
+                            <option value="Pasta">Pasta</option>
+                            <option value="Noodles">Noodles</option>
                         </select>
                     </div>
                     <div className="admin-form-product-price admin-form-item">
