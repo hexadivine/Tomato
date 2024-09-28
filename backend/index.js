54eecd83aa0ba0ec65272ee007fbb8a2fea3e5ff
hexadivine <hexadivine@gmail.com>
2024-09-28 21:23:28 +1000

    .

--- backend/index.js
+++ backend/index.js
@@ -22,15 +22,12 @@ app.get("/", (req, res) => {
     res.send({ success: true, msg: "Server is up and running" });
 });
 
-// app.listen(process.env.PORT || 9999, () => {
-//     console.log("Server is up and running");
-// });
-
 // run
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    app.listen(process.env.PORT || 9999, () => {
-        console.log("Server is up and running");
-    });
+    console.log("connected");
+    // app.listen(process.env.PORT || 9999, () => {
+    //     console.log("Server is up and running");
+    // });
 });
 
 export default app;
